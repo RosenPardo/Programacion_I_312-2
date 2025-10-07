59e62055a132df46f2d590020f82bd95d75b42d4
RosenPardo <rosen.pardox@gmail.com>
2025-10-07 12:07:10 -0300

    Se ajusta el módulo configuraciones.

--- Progra_I_PP/configuraciones.py
+++ Progra_I_PP/configuraciones.py
@@ -1,14 +1,4 @@
 
-"""cantidad_estudiantes = 3
-calificaciones = inicializar_matriz(cantidad_estudiantes, 5, 0)
-legajo_estudiante = [0] * cantidad_estudiantes
-nombre_estudiante = [0] * cantidad_estudiantes
-genero_estudiante = [0] * cantidad_estudiantes
-estado_legajo = [0] * cantidad_estudiantes
-"""
-
-#Comentar las siguientes líneas y descomentar las líneas de arriba, si se quiere probar la opción 1 del menú:
-
 calificaciones = [[2,10,8,7,9], [8,10,5,6,7], [4,10,7,9,8], [9,6,8,10,7], [5,7,9,8,6], [7,8,6,7,8], [6,7,5,9,8], [10,9,8,7,10], [9,8,9,7,9], [7,6,8,5,9], [9,9,10,8,9], [6,8,7,9,6], [4,5,7,6,5], [8,8,9,7,8], [10,10,9,9,10], [5,6,7,6,5], [5,6,4,7,6], [7,8,6,9,8], [9,8,10,9,10], [3,5,4,6,5], [8,7,9,8,7], [10,7,9,8,9], [6,5,8,7,6], [8,9,10,9,8], [7,8,9,8,9], [5,6,5,7,6], [5,4,6,5,4], [0,0,0,0,0], [0,0,0,0,0], [0,0,0,0,0]]
 
 legajo_estudiante = [112233, 332211, 445566, 556677, 778899, 889900, 990011, 110022, 220033, 330044, 440055, 550066, 660077, 770088, 880099, 990100, 100111, 200222, 300333, 400444, 500555, 600666, 700777, 800888, 900999, 111222, 222111, 0, 0, 0]
